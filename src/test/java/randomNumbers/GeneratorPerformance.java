b7b49b13645f677b78e65d0569a185f2720bccd8
Johannes Link <business@johanneslink.net>
2020-05-02 10:32:07 +0200

    gg

--- src/test/java/randomNumbers/GeneratorPerformance.java
+++ src/test/java/randomNumbers/GeneratorPerformance.java
@@ -1,6 +1,7 @@
 package randomNumbers;
 
 import java.util.Random;
+import java.util.concurrent.*;
 
 import net.jqwik.api.*;
 
@@ -12,27 +13,40 @@ public class GeneratorPerformance {
 		Arbitrary<Integer> integers = Arbitraries.integers().between(-100000, 100000);
 
 		RandomGenerator<Integer> generator = integers.generator(10000);
-		measure("jqwik", generator);
+		measure("jqwik", generator, new Random());
 	}
 
 	@Example
 	@Label("gaussian: -100000 .. 100000")
 	void gaussianMin100000to100000() {
 		RandomGenerator<Integer> generator = new GaussianGenerator(-100000, 100000);
-		measure("gaussian", generator);
+		measure("gaussian", generator, new Random());
 	}
 
 	@Example
 	@Label("simple: -100000 .. 100000")
 	void simpleMin100000to100000() {
 		RandomGenerator<Integer> generator = new SimpleGenerator(-100000, 100000);
-		measure("simple", generator);
+		measure("simple", generator, new Random());
 	}
 
-	private void measure(String label, RandomGenerator<Integer> generator) {
-		Random random = new Random();
+	@Example
+	@Label("fast: -100000 .. 100000")
+	void fastMin100000to100000() {
+		RandomGenerator<Integer> generator = new SimpleGenerator(-100000, 100000);
+		measure("fast", generator, new FastRandom());
+	}
+
+	@Example
+	@Label("threadLocal: -100000 .. 100000")
+	void threadLocalMin100000to100000() {
+		RandomGenerator<Integer> generator = new SimpleGenerator(-100000, 100000);
+		measure("thread local", generator, ThreadLocalRandom.current());
+	}
+
+	private void measure(String label, RandomGenerator<Integer> generator, Random random) {
 		long before = System.currentTimeMillis();
-		int tries = 10000000;
+		int tries = 50000000;
 		for (int i = 0; i < tries; i++) {
 			generator.next(random);
 		}
